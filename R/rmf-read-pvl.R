91ae81e4762c748d3356fec048e50f54467f95a7
Alberto Casillas <ingcasillastrasvina@gmail.com>
2019-03-07 14:27:02 +0100

    Update rmf-read-pvl.R

--- R/rmf-read-pvl.R
+++ R/rmf-read-pvl.R
@@ -9,27 +9,27 @@
 #' @export
 rmf_read_pvl <- function(file = {cat('Please select pvl file ...\n'); file.choose()},
                          read_all=F) {
-  
-  pvl_lines <- read_lines(file)
-  pvl <- list()
-  
-  # data set 0
-    data_set_0 <- rmfi_parse_comments(pvl_lines)
-    comment(pvl) <- data_set_0$comments
-    pvl_lines <- data_set_0$remaining_lines
-    rm(data_set_0)
-  
-  # data set 1
-    ifelse(read_all, pvl$np <- length(pvl_lines)-1, pvl$np <- as.numeric(pvl_lines[1]))
-    pvl_lines <- pvl_lines[-1]
-  
-  # data set 2
-    for(i in 1:pvl$np) {
+   
+   pvl_lines <- read_lines(file)
+   pvl <- list()
+   
+   # data set 0
+   data_set_0 <- rmfi_parse_comments(pvl_lines)
+   comment(pvl) <- data_set_0$comments
+   pvl_lines <- data_set_0$remaining_lines
+   rm(data_set_0)
+   
+   # data set 1
+   ifelse(read_all, pvl$np <- length(pvl_lines)-1, pvl$np <- as.numeric(pvl_lines[1]))
+   pvl_lines <- pvl_lines[-1]
+   
+   # data set 2
+   for(i in 1:pvl$np) {
       pvl$parnam[i] <- as.character(strsplit(pvl_lines[1],' ')[[1]][1])
       pvl$parval[i] <- as.numeric(rmfi_remove_empty_strings(strsplit(pvl_lines[1],' ')[[1]])[2])
       pvl_lines <- pvl_lines[-1]
-    }
-
+   }
+   
    class(pvl) <- c('pvl','rmf_package')
    return(pvl)
 }
@@ -37,6 +37,6 @@ rmf_read_pvl <- function(file = {cat('Please select pvl file ...\n'); file.choos
 #' @describeIn rmf_read_pvl Deprecated function name
 #' @export
 read_pvl <- function(...) {
-  .Deprecated(new = "rmf_read_pvl", old = "read_pvl")
-  rmf_read_pvl(...)
+   .Deprecated(new = "rmf_read_pvl", old = "read_pvl")
+   rmf_read_pvl(...)
 }
